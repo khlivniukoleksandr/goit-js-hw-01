544710eb859b9679f8ba2210a5cb76e1a7d2897c
Khlivniuk Oleksandr <sasha2056083@gmail.com>
2025-07-18 20:48:45 +0300

    Solution!

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+  const contentNum = Number.parseFloat(content);
+  const paddingNum = Number.parseFloat(padding);
+  const borderNum = Number.parseFloat(border);
+  return contentNum + (paddingNum + borderNum) * 2;
+}
+
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
